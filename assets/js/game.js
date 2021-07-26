19dfed920159e53e6671647b14c7523ca1ca1cac
twschan <wingci13@gmail.com>
2021-07-26 16:28:24 +0000

    script: removed written script

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,5 @@
 // Card data
 // Display 10 cards with different value
-const cardsArray = [
-    { name: "tea", img: "./assets/images/iced-tea.png", },
-    { name: "matcha", img: "./assets/images/matcha.png", },
-    { name: "mochi", img: "./assets/images/mochi.png", },
-    { name: "onigiri", img: "./assets/images/onigiri.png", },
-    { name: "ramen", img: "./assets/images/ramen.png", },
-    { name: "rice", img: "./assets/images/rice.png", },
-    { name: "chopsticks", img: "./assets/images/sticks.png", },
-    { name: "sushi", img: "./assets/images/sushi.png", },
-    { name: "takoyaki", img: "./assets/images/takoyaki.png", },
-    { name: "tempura", img: "./assets/images/tempura.png", },
-];
+
 
 
