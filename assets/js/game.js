de95a85d758f286cd53d5af569ae3e068d8a1d42
twschan <wingci13@gmail.com>
2021-07-29 18:04:15 +0000

    script: add array of objects to create the cards and global variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,21 @@
-// Card data
+// List of possible cards
 // Display 10 cards with different value
+let baseCards = ['chopsticks', 'iced-tea', 'matcha', 'mochi', 'onigiri', 'ramen', 'rice', 'sushi', 'takoyaki', 'tempura'];
 
+// Duplicate array items to make pairs
+let possibleCards = baseCards.concat(baseCards); 
 
+// Global variables
+const numCards = possibleCards.length;
+const maxMatch = baseCards.length; // Maximum pairs
+let opened = [];
+let numMatch = 0;
+let numMoves = 0;
 
+// Timers
+let seconds = 0;
+let minutes = 0;
+let t;
+
+// Shuffle
+// source: http://stackoverflow.com/a/2450976
